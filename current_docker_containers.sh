71f89758cd7052e253df07b681bf83695e14a61d
adityap <adityap@tierra.net>
2017-02-04 17:01:41 -0800

    added a measurement to check if any containers are down for summary purposes

--- current_docker_containers.sh
+++ current_docker_containers.sh
@@ -7,13 +7,23 @@ PORT='8086'
 DB='telegraf'
 MEASUREMENT='docker_container_status'
 MEASUREMENT_HOST=''
+
+# simple measurement of whether any containers are down.
+MEASUREMENT_UP='docker_containers_running'
 ## END_CONFIG ##
 
 # the awk skips the first line of output, which are the column headers
-docker ps -a --format "table {{.Names}}\t{{.Status}}" | awk 'NR > 1' |
+OUTPUT="$(docker ps -a --format "table {{.Names}}\t{{.Status}}" | awk 'NR > 1')"
   while read i
   do
     container_name=$(awk '{print $1}' <<< "$i")
     container_status=$(awk '{print $2=="Up"?1:0}' <<< "$i")
+
+    if [ $container_status -eq 0 ]; then
+        all_containers_up=0
+    fi
+
     curl -s -i -XPOST "http://$INFLUX_HOST:$PORT/write?db=$DB" --data-binary "$MEASUREMENT,host=$MEASUREMENT_HOST,container=$container_name status=$container_status"
-  done
+  done <<< "$OUTPUT"
+
+  curl -s -i -XPOST "http://$INFLUX_HOST:$PORT/write?db=$DB" --data-binary "$MEASUREMENT_UP,host=$MEASUREMENT_HOST status=$all_containers_up"
