8c4556b040cac97742867463a5f94413d8b84fe2
Aditya Pandurangi <aditya.pandurangi@gmail.com>
2018-11-20 22:46:41 -0800

    Update scripts with batching (#2)
    * updated scripts to include batching for influx db calls to prevent timestamps being off by a few ms. 
    added a new script for querying sensors

--- disk_smart_status.sh
+++ disk_smart_status.sh
@@ -1,23 +1,29 @@
 #
-# Hard Disk Power On Hours
+# Disk SMART Status
 #
 # Added -n standby to the check so smartctl is not spinning up my drives
-
+#
+#
+# Set Vars
+#
 ### CONFIG ###
-# all fields required
 INFLUX_HOST=''
 PORT='8086'
 DB=''
 MEASUREMENT='hd_smart_status'
 MEASUREMENT_HOST=''
-DISK_ARRAY=( sdc sdd sde sdf sdb )
-DESCRIPTION=( parity disk1 disk2 disk3 cache )
+DISK_ARRAY=( sdi sdd sdh sdc sdg )
+DESCRIPTION=( disk1 disk2 disk3 disk4 disk5 )
+HUMAN_READABLE_DESCRIPTION=( "Disk\ 1" "Disk\ 2" "Disk\ 3" "Disk\ 4" "Disk\ 5" )
 ### END CONFIG ###
+
+## hd smart status
+post_url=
 
 i=0
 for DISK in "${DISK_ARRAY[@]}"
 do
-    smartctl -n standby -H /dev/${DISK} | grep -E "self-assessment" | awk '{ print $6 }' | while read SMARTSTATUS
+    while read SMARTSTATUS;
     do
         if [ "$SMARTSTATUS" = "PASSED" ]; then
             value=1
@@ -25,8 +31,9 @@ do
             value=2;
         fi
 
-        curl -s -i -XPOST "http://$INFLUX_HOST:$PORT/write?db=$DB" --data-binary "$MEASUREMENT,host=$MEASUREMENT_HOST,disk=${DESCRIPTION[$i]} value=$value"
-        #echo ${DESCRIPTION[$i]} $value
-    done
+        post_url=$post_url"$MEASUREMENT,host=$MEASUREMENT_HOST,hr_host=${HUMAN_READABLE_DESCRIPTION[$i]},disk=${DESCRIPTION[$i]} value=$value"$'\n'
+        #echo "$post_url"
+    done <<<$(smartctl -n standby -H /dev/${DISK} | grep -E "self-assessment" | awk '{ print $6 }')
     ((i++))
 done
+curl -s -i -XPOST "http://$INFLUX_HOST:$PORT/write?db=$DB" --data-binary "$post_url"
